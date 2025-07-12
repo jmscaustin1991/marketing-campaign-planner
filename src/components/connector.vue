c5dccfdb8708010b1d4b5afe935f86ce20af65a5
jmscaustin1991 <167023595+jmscaustin1991@users.noreply.github.com>
2025-07-12 17:40:03 -0500

    Create connector.vue

---
+++ src/components/connector.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg class="connector-layer">
+    <path
+      v-for="line in lines"
+      :key="line.id"
+      :d="line.path"
+      stroke="#333" fill="none"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: ['cards'],
+  computed: {
+    lines() {
+      // Compute elbow connectors between cards sharing same label prefix
+      return [];
+    }
+  }
+};
+</script>
+
+<style scoped>
+.connector-layer { position: absolute; top: 0; left: 0; width: 100%; height: 100%; pointer-events: none; }
+</style>
